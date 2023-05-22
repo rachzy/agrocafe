e6dd11213468c37530300b2b32d19c8e1398e1d5
Henrique <henriqueferreira.fox@gmail.com>
2023-05-22 18:17:27 -0300

    cria o slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Header from "./components/Header";
+import Slider from "./components/Slider";
+
+import Slide1 from "./assets/imagens/sliders/slider1.jpg";
+import Slide2 from "./assets/imagens/sliders/slider2.jpg";
+import Slide3 from "./assets/imagens/sliders/slider3.jpg";
+import Slide4 from "./assets/imagens/sliders/slider4.jpg";
 
 const App = () => {
+  const slides = [Slide1, Slide2, Slide3, Slide4];
   return (
     <main>
       <Header />
+      <Slider images={slides} floatingDots />
     </main>
   );
 };
