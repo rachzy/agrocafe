9ae1b39374ccf33d1ccbcdc4a695929d161bdaa8
Henrique <henriqueferreira.fox@gmail.com>
2023-05-27 12:05:59 -0300

    cria ScrollBanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import Header from "./components/Header";
 import Slider from "./components/Slider";
 import Descricao from "./sections/Descricao";
@@ -9,16 +11,26 @@ import Slide2 from "./assets/imagens/sliders/slider2.jpg";
 import Slide3 from "./assets/imagens/sliders/slider3.jpg";
 import Slide4 from "./assets/imagens/sliders/slider4.jpg";
 import Expertise from "./sections/Expertise";
+import ScrollBanner from "./sections/ScrollBanner";
+
+import Banner1 from "./assets/imagens/imagens-scroll/imagem1.png";
+import Slogan1 from "./assets/imagens/imagens-scroll/texto1.png";
+
+import Banner2 from "./assets/imagens/imagens-scroll/imagem2.png";
+import Slogan2 from "./assets/imagens/imagens-scroll/texto2.png";
 
 const App = () => {
   const slides = [Slide1, Slide2, Slide3, Slide4];
+
   return (
     <main>
       <Header />
       <Slider images={slides} floatingDots />
       <Descricao />
       <Expertise />
+      <ScrollBanner banner={Banner1} slogan={Slogan1} />
       <FAQ />
+      <ScrollBanner banner={Banner2} slogan={Slogan2} />
       <Contact />
     </main>
   );
